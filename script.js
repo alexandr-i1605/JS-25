6b63a9117dbb342fac80b337e4f195f48f9bf43c
Alexandr <alexandr1605.i@yandex.ru>
2025-03-17 20:47:49 +0300

    added new calc operations, extrabutton, backspace button, updated buttons location

--- script.js
+++ script.js
@@ -55,14 +55,47 @@ window.onload = function(){
         selectedOperation = '%'
     }
 
-    // кнопка очищения
-    document.getElementById("btn_op_clear").onclick = function() { 
+    //уделение всего
+    function AllClear() {
         a = ''
         b = ''
         selectedOperation = ''
         expressionResult = ''
         outputElement.innerHTML = 0
     }
+
+    //удаление последней цифры
+    function BackSpace() {
+        if (!selectedOperation) {
+            a = (a).toString()
+            if (a.length > 0) {
+                a = a.slice(0, -1)
+                outputElement.innerHTML = a || 0
+            }
+        } else {
+            b = (b).toString()
+            if (b.length > 0) {
+                b = b.slice(0, -1)
+                outputElement.innerHTML = b || 0
+            }
+        }
+    }
+
+    document.getElementById("btn_op_clear").addEventListener('mousedown', function() {
+        timer = setTimeout(function() {
+            AllClear()
+        }, 600)
+    });
+
+     //окончание нажатия
+     document.getElementById("btn_op_clear").addEventListener('mouseup', function() {
+        clearTimeout(timer)
+    });
+
+    //1 клик
+    document.getElementById("btn_op_clear").addEventListener('click', function() {
+        BackSpace()
+    });
 
     document.getElementById("btn_op_sign").onclick = function() {
         if (!selectedOperation) {
@@ -73,6 +106,68 @@ window.onload = function(){
         }
     }
 
+    document.getElementById("btn_op_sqrt").onclick = function() {
+        if (!selectedOperation) {
+            if (a !== '') {
+                a = Math.sqrt(a)
+                outputElement.innerHTML = a
+            }
+        }
+    }
+
+    document.getElementById("btn_op_pow").onclick = function() {
+        if (!selectedOperation) {
+            if (a !== '') {
+                a = a*a
+                outputElement.innerHTML = a
+            }
+        }
+    }
+
+    const fact = n => {
+        let res = 1;
+        for (let i = 2; i <= n; i++) {
+            res *= i;
+        }
+        return res;
+    };
+
+    document.getElementById("btn_op_fact").onclick = function() {
+        if (!selectedOperation) {
+            if (a !== '') {
+                a = fact(a)
+                outputElement.innerHTML = a
+            }
+        }
+    }
+
+    document.getElementById("btn_op_m3").onclick = function() {
+        if (!selectedOperation) {
+            if (a !== '') {
+                a = a*1000
+                outputElement.innerHTML = a
+            }
+        }
+    }
+
+    document.getElementById("btn_op_ln").onclick = function() {
+        if (!selectedOperation) {
+            if (a !== '') {
+                a = Math.log(a)
+                outputElement.innerHTML = a
+            }
+        }
+    }
+
+    //дополнительные кнопок
+    document.getElementById("btn_advanced").onclick = function() {
+        const advbtns = document.getElementById("adv_btns")
+        if (advbtns.style.display === "none") {
+            advbtns.style.display = "flex"
+        } else {
+            advbtns.style.display = "none"
+        }
+    }
     // кнопка расчёта результата
     document.getElementById("btn_op_equal").onclick = function() { 
         if (a === '' || b === '' || !selectedOperation)
