94299ee1d8d3f2b9d97851b3e85a3b5319cd6f6e
Alexandr <alexandr1605.i@yandex.ru>
2025-04-23 22:02:03 +0300

    added filter buttons(radios)

--- pages/main/index.js
+++ pages/main/index.js
@@ -23,7 +23,7 @@ export class MainPage {
                         font-size: 16px;
                         font-weight: 700;">Домой</button>
             </header>
-            <div class="d-flex mb-3">
+            <div class="d-flex flex-row mb-3">
                 <button class="btn btn-success me-2" id="add-card-btn"style="border-radius: .4rem;
                         background: #e4002b;
                         color:#ffffff;
@@ -32,8 +32,61 @@ export class MainPage {
                         font-weight: 700;">
                     <i class="bi bi-plus-circle" ></i> Добавить карточку
                 </button>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="Radios" id="AllCards" value="option1" checked>
+                    <label class="form-check-label" for="AllCards">
+                        Все карточки
+                        </label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="Radios" id="CleanCards" value="option2">
+                    <label class="form-check-label" for="CleanCards">
+                        Скрыть неполные карточки
+                    </label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="Radios" id="Palindroms" value="option3">
+                    <label class="form-check-label" for="Palindroms">
+                        Найти палиндромы
+                    </label>
+                </div>
+
+                <div class="input-group me-3" style="width: 250px;">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text p-0 border-0 bg-transparent">
+                            <input class="form-check-input mt-0" type="radio" name="Radios" id="Splitter" value="option4" 
+                            style="margin-left: 10px; margin-right: 5px;">
+                        </div>
+                    </div>
+                    <input type="text" class="form-control" placeholder="Разделитель" 
+                    style="border-radius: 0 .4rem .4rem 0;">
+                </div>
+
+                <div class="input-group" style="width: 250px;">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text p-0 border-0 bg-transparent">
+                            <input class="form-check-input mt-0" type="radio" name="Radios" id="Coupl" value="option5" 
+                            style="margin-left: 10px; margin-right: 5px;">
+                        </div>
+                    </div>
+                    <input type="text" class="form-control" placeholder="Найти пару" 
+                    style="border-radius: 0 .4rem .4rem 0;">
+                </div>
             </div>
             <div id="main-page" class="row row-cols-1 row-cols-md-3 g-4"></div>
+
+            <style>
+                .form-check-input:checked[type=radio] {
+                    background-color: #e4002b;
+                    border-color: #e4002b;
+                }
+                .form-check-input:focus[type=radio] {
+                    box-shadow: 0 0 0 1px #fff, 0 0 0 0.2rem rgba(255, 0, 13, 0.25);
+                }
+                .input-group-text {
+                    background-color: transparent;
+                }
+            </style>
         `;
     }
 
@@ -67,7 +120,7 @@ export class MainPage {
             {
                 id: 4,
                 src: "https://s82079.cdn.ngenix.net/330x0/7m7ja14p9nuufnl2hz2c5ts65252",
-                title: "Taco-cat",
+                title: "TacoCat",
                 text: "200"
             }
         ];
