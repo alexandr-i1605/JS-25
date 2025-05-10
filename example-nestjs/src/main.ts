4201d523792a7511e266ed6d3b73ea3b1ac6995d
Александр Иванов <Alexandr1605.i@yandex.ru>
2025-05-10 20:07:29 +0300

    updated CORS params

--- example-nestjs/src/main.ts
+++ example-nestjs/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: ['http://127.0.0.1:5500', 'http://localhost:3000'],
+    methods: "*",
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
