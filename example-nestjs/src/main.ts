e808c60aa409e4e54615692ee6d5daef25749a6a
Alexandr <alexandr1605.i@yandex.ru>
2025-05-13 22:42:00 +0300

    deleted CORS params

--- example-nestjs/src/main.ts
+++ example-nestjs/src/main.ts
@@ -4,11 +4,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  app.enableCors({
-    origin: ['http://127.0.0.1:5500', 'http://localhost:3000'],
-    methods: "*",
-  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
